8361331104ee1995458cf11003efa1bba5efb3f5
Leilasi <leilafernandadasilva1994@gmail.com>
2025-02-10 23:04:09 -0300

    "Adicionando Logs nos erros de autenticação e autorização"

--- src/main/java/com/cadastro/relacional/empresa/config/security/CustomAccessDeniedHandler.java
+++ src/main/java/com/cadastro/relacional/empresa/config/security/CustomAccessDeniedHandler.java
@@ -2,6 +2,8 @@ package com.cadastro.relacional.empresa.config.security;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -11,9 +13,12 @@ import java.io.IOException;
 @Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomAccessDeniedHandler.class);
+
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
                        AccessDeniedException accessDeniedException) throws IOException {
+        LOGGER.error(accessDeniedException.getMessage(), accessDeniedException);
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         response.setCharacterEncoding("UTF-8");
         response.setContentType("application/json");
